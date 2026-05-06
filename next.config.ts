fbeed82b672686d34a8eb335e649f77c42bf6a1a
usail7860 <usail7860@users.noreply.github.com>
2026-05-06 12:23:08 +0530

    update next

--- next.config.ts
+++ next.config.ts
@@ -11,12 +11,10 @@ const nextConfig: NextConfig = {
   //     },
   //   ],
   // },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
 };
 
 export default nextConfig;
+ 
